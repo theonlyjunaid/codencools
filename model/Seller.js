bb92626c0f979b1e2ebf87d776a370eedb6a3d32
junaid <onlyjunaid2002@gmail.com>
2023-01-07 01:56:20 +0530

    backend added

---
+++ model/Seller.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const SellerSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: 'seller' },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+    },
+    rating: {
+        type: Number,
+    },
+
+    numReviews: {
+        type: Number,
+    },
+
+    isFeatured: {
+        type: Boolean,
+    },
+
+}, { timestamps: true });
+
+mongoose.models = {};
+export default mongoose.model('Seller', SellerSchema);
---
+++ pages/api/adduser.js
@@ -0,0 +1,22 @@
+import User from "../../model/User";
+import connectDB from "../../middleware/mongoose";
+var CryptoJS = require("crypto-js");
+
+const handler = async (req, res) => {
+    if (req.method === 'POST') {
+        const { name, email } = req.body;
+        let user = await User.findOne({ email: email });
+        if (user) {
+            res.status(400).json({ success: false, message: 'User already exists' });
+        } else {
+            let u = new User({ name, email, password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString() });
+            await u.save();
+            res.status(200).json({ success: true, message: 'Account Created successfully Now you can login' });
+        }
+    }
+    else {
+        res.status(400).json({ message: 'This method is not allowed' });
+    }
+}
+
+export default connectDB(handler);
