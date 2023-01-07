697d62ee95636e3a86524648f8fb62dab9fb854a
Deepanshu Attri <iamattri0001@gmail.com>
2023-01-07 18:07:31 +0530

    update UI

--- pages/checkout.js
+++ pages/checkout.js
@@ -12,23 +12,23 @@ function checkout() {
             </div>
             <div className="cheack-info pb-10">
                 <p className="text-xl mb-6">Personal Details</p>
-                <div className="input mb-4">
-                    <label htmlFor="email">Email Address</label>
-                    <input type="text" className="p-2 border w-full" name="email"/>
+                <div className="input mb-6">
+                    <label htmlFor="email" className='mb-2'>Email Address</label>
+                    <input type="text" id='email' className="p-2 border border-gray-400 w-full" name="email"/>
                 </div>
-                <div className="input mb-4">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" className="p-2 border w-full" name="name"/>
+                <div className="input mb-6">
+                    <label htmlFor="name" className='mb-2'>Name</label>
+                    <input type="text" id='name' className="p-2 border border-gray-400 w-full" name="name"/>
                 </div>
-                <div className="input mb-4">
-                    <label htmlFor="email">Contact Number</label>
-                    <input type="text" className="p-2 border w-full" name="phone"/>
+                <div className="input mb-6">
+                    <label htmlFor="contact" className='mb-2'>Contact Number</label>
+                    <input type="text" id='contact' className="p-2 border border-gray-400 w-full" name="phone"/>
                 </div>
-                <div className="input mb-4">
-                    <label htmlFor="address">Address:</label>
-                    <textarea name="address" className="border w-full h-48"></textarea>
+                <div className="input mb-6">
+                    <label htmlFor="address" className='mb-2'>Address:</label> 
+                    <textarea name="address" id='address' className="border w-full border-gray-400" rows='2'></textarea>
                 </div>
-                <button className="btn w-full bg-gray-800 hover:bg-gray-700 p-2 py-3 text-white rounded-lg">Proceed to Checkout 👜</button>
+                <button className="btn mt-5 w-full bg-gray-800 hover:bg-gray-700 p-2 py-3 text-white rounded-lg">Proceed to Checkout 👜</button>
             </div>
         </div>
 
