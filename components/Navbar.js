77d5a55e7e03f2ffe9ef0d0d7bf50ea12bf4b442
Ashish Jha <88213707+ashjha03@users.noreply.github.com>
2023-01-08 02:13:43 +0530

    nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -30,23 +30,23 @@ function Navbar({cart, addToCart, removeFromCart}) {
             <span className="ml-3 text-xl">QuickIT</span>
           </Link>
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            <Link href="/" className="mr-5 hover:text-gray-900 cursor-pointer">
+            <Link href="/" className="mr-5 hover:text-gray-400 cursor-pointer">
               Home
             </Link>
             <Link
+              href="/about"
+              className="mr-5 hover:text-gray-400 cursor-pointer"
+            >
+              About
+            </Link>
+            <Link
               href="/viewProducts"
-              className="mr-5 hover:text-gray-900 cursor-pointer"
+              className="mr-5 hover:text-gray-400 cursor-pointer"
             >
               View Products
             </Link>
-            <Link href="#" className="mr-5 hover:text-gray-900 cursor-pointer">
-              Track
-            </Link>
-            <Link
-              href="/about"
-              className="mr-5 hover:text-gray-900 cursor-pointer"
-            >
-              About
+            <Link href="/addproduct" className="mr-5 hover:text-gray-400 cursor-pointer">
+              Add Product
             </Link>
           </nav>
           <Link href='/login' className="text-white inline-flex items-center bg-gray-700 border-0 py-1 px-3 mr-4 focus:outline-none rounded text-base mt-4 md:mt-0">
