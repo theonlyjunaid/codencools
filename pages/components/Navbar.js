8af651e3dc808597865753f3d34de2c067b1e1a2
Deepanshu Attri <iamattri0001@gmail.com>
2023-01-07 16:22:58 +0530

    Update home page

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 function Navbar() {
     return (
-        <header className="bg-gray-300 text-gray-600 body-font  border-b-2 border-blue-600">
+        <header className="text-gray-600 body-font border-b-2">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
