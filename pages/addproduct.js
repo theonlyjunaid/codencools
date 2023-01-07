41422a9ede6da36702e8f682b2598980b4e9f1c9
kunal00000 <kunalverma2468@gmail.com>
2023-01-07 13:57:59 +0530

    changes

--- pages/addproduct.js
+++ pages/addproduct.js
@@ -48,12 +48,29 @@ const addproduct = () => {
                                     htmlFor="visitors"
                                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                 >
-                                    Recomended Price
+                                    MRP
                                 </label>
                                 <input
                                     type="number"
                                     id="visitors"
-                                    className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder=""
+                                    required=""
+                                />
+                            </div>
+                        </span>
+                        <span className="">
+                            <div>
+                                <label
+                                    htmlFor="visitors"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Base Price
+                                </label>
+                                <input
+                                    type="number"
+                                    id="visitors"
+                                    className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder=""
                                     required=""
                                 />
@@ -70,7 +87,7 @@ const addproduct = () => {
                             <input
                                 type="number"
                                 id="visitors"
-                                className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder=""
                                 required=""
                             />
